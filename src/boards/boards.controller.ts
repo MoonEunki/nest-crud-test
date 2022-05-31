b7fcfbb5a64ff1364a8cca54e00f166176427421
mek <ansenfrl@gmail.com>
2022-05-31 18:34:43 +0900

    전체 게시글 불러오는 service 생성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,7 +1,14 @@
 import { BoardsService } from './boards.service';
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
+
+  @Get()
+  getAllBoards() {
+    return this.boardsService.getAllBoards();
+  }
+
+
 }
