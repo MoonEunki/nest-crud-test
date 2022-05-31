b55997ec875bafdcc2e531a567ffd7cab621fdf0
mek <ansenfrl@gmail.com>
2022-05-31 19:05:35 +0900

    게시글 없는경우 예외처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,6 @@
 import { CreateBoardDto } from './dto/create-board-dto';
 import { Board, BoardStatus } from './board.model';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 
 @Injectable()
@@ -25,7 +25,9 @@ export class BoardsService {
   }
 
   getBoardById(id:string):Board{
-    return this.boards.find((board)=> board.id === id);
+    const foundBoard = this.boards.find((board)=> board.id === id);
+    if(!foundBoard) throw new NotFoundException(`${id} 게시글을 찾을수없습니다.`)
+    return foundBoard;
   }
 
   deleteBoardById(id:string):void{
